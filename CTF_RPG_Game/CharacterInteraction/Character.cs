4a09e9eaae950305cb5ff3d5b9fa7323492b5d1f
bogotolec <bogotolec@inbox.ru>
2017-07-10 22:27:22 +0700

    Added nachal of characters

---
+++ CTF_RPG_Game/CharacterInteraction/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF_RPG_Game.CharacterInteraction
+{
+    class Character
+    {
+        public int ID { get; }
+        public string Name { get; set; }
+        public int Level { get; }
+        private int X;
+        private int Y;
+        
+
+
+    }
+}
