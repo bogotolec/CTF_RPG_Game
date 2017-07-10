55e7d2b0ad98feda4a5bf82a4bdfb06192270db2
bogotolec <bogotolec@inbox.ru>
2017-07-10 21:50:43 +0700

    Little changes

---
+++ CTF_RPG_Game/ClientInteraction/ConnectionManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using CTF_RPG_Game_Server;
+
+namespace CTF_RPG_Game.ClientInteraction
+{
+    class ConnectionManager
+    {
+        private static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Program.PORT);
+        private static Socket ListenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        // !!!SINGLETON WARNING!!!
+        private static ConnectionManager instance;
+
+        static ConnectionManager()
+        { }
+
+        public static ConnectionManager GetManager()
+        {
+            if (instance == null)
+            {
+                instance = new ConnectionManager();
+            }
+            return instance;
+        }
+        // !!!END OF SINGLETON WARNING!!!
+
+        private static void Listen()
+        {
+            try
+            {
+                // Start listening
+                ListenSocket.Bind(ipPoint);
+                ListenSocket.Listen(32);
+
+                // Socket handler
+                while (true)
+                {
+                    Socket handler = ListenSocket.Accept();
+                    
+                    /*
+                     * TODO
+                     */
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                ListenSocket.Close();
+            }
+        }
+    }
+}
--- CTF_RPG_Game/Program.cs
+++ CTF_RPG_Game/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CTF_RPG_Game_Server
 {
     class Program
     {
+        public static int PORT;
+        public static string CONFIGURATION_FILE = "config";
+
         static void Main(string[] args)
         {
+            InitializeServerConfiguration();
+        }
+
+        private static void InitializeServerConfiguration()
+        {
+            string[] configStrings = File.ReadAllText(CONFIGURATION_FILE).ToLower().Split('\n');
+            foreach (var cString in configStrings)
+            {
+                if (cString.StartsWith("#"))
+                    continue;
+
+                else if (cString.StartsWith("port="))
+                    PORT = int.Parse(cString.Substring(5));
+            }
         }
     }
 }
