e6fd5a3103e946b20b01eb548693bb65bf873656
bogotolec <bogotolec@inbox.ru>
2017-07-12 16:18:41 +0700

    Added base for items.

---
+++ CTF_RPG_Game/CharacterInteraction/IItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF_RPG_Game.CharacterInteraction
+{
+    interface IItem
+    {
+        int Id { get; }
+        string Name { get; }
+        string Description { get; }
+        int SellCost { get; }
+        int BuyCost { get; }
+        void Sell();
+        void Buy();
+    }
+
+    interface IEquiped
+    {
+        string EquipDescription { get; }
+        EquipSlot Slot { get; }
+    }
+
+    enum ItemState { Equiped, EqupedAndUsable, Usable, UsableOnce }
+    enum EquipSlot { Head, Body, LHand, Rhand, Boots, Jewelery }
+}
