b76bae40f9d2fbff88b63e9db631ca7cd5fde318
bogotolec <bogotolec@inbox.ru>
2017-07-12 13:42:46 +0700

    Added interface for skils

---
+++ CTF_RPG_Game/CharacterInteraction/Skill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF_RPG_Game.CharacterInteraction
+{
+    interface ISkill
+    {
+        int Id { get; }
+        string Name { get; }
+        string Description { get; }
+        int PointsToLearn { get; } 
+        SkillState State { get; }
+        CharacterClass Class { get; }
+        ISkill[] NeededSkills { get; }
+    }
+
+    enum SkillState { Passive, Active };
+    enum CharacterClass { Hacker, SocEng }
+}
