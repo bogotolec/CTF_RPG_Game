1c76f38642945568c1adb30d9794daba5a3bfe09
bogotolec <bogotolec@inbox.ru>
2017-07-12 17:37:29 +0700

    Added language example

---
+++ CTF_RPG_Game/Languages/Russian.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF_RPG_Game.Languages
+{
+    class Russian : ILanguage
+    {
+        public string RegistrationOrLoginText { get { return "Вы уже зарегистрированы?\nВведите \"Y\", если да, или \"N\", если нет."; } }
+    }
+}
