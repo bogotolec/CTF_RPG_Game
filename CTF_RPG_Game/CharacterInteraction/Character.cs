0808f19ea59edd771882313fa34511adc134f829
bogotolec <bogotolec@inbox.ru>
2017-07-12 13:16:36 +0700

    Added skillpoints

--- CTF_RPG_Game/CharacterInteraction/Character.cs
+++ CTF_RPG_Game/CharacterInteraction/Character.cs
@@ -5,16 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CTF_RPG_Game.CharacterInteraction
-{
+{ 
     class Character
     {
         public int ID { get; }
         public string Name { get; set; }
         public int Level { get; }
-        private int X;
-        private int Y;
-        
-
+        public int X { get; }
+        public int Y { get; }
+        public int SkillPoints { get; }
 
     }
 }
