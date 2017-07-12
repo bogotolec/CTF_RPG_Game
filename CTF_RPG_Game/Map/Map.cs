24e8d5df91302fd8cb057774461f9d2365b38279
Frodan <Frodan@users.noreply.github.com>
2017-07-12 14:02:51 +0700

    Added Map and Cell

---
+++ CTF_RPG_Game/Map/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF_RPG_Game.Map
+{
+    class Map
+    {
+        public int Weight;
+        public int Height;
+        public int MapId;
+
+    }
+}
