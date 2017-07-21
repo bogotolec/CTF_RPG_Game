291ed016bb656ae803a1a98f6460f71cb6e306f0
bogotolec <bogotolec@inbox.ru>
2017-07-21 13:45:33 +0700

    Изменения Frodan'а

--- CTF_RPG_Game/MapComponents/Cell..cs
+++ CTF_RPG_Game/MapComponents/Cell..cs
@@ -14,11 +14,51 @@ namespace CTF_RPG_Game.MapComponents
         public bool IsTaskable;
         public bool IsTeleport;
         public bool IsVisibleWithSkills;
-        public char Symbol;
+        public string Symbol;
+        public string Color;
+        private static Dictionary<string, string> ColorDict = new Dictionary<string, string>();
+        private static Dictionary<string, string> SymbolDict = new Dictionary<string, string>();
+
+        private void CreateDict()
+        {
+            ColorDict.Add("red", "0001");
+            ColorDict.Add("green", "0010");
+            ColorDict.Add("blue", "0100");
+            ColorDict.Add("yellow", "1000");
+            ColorDict.Add("black", "0011");
+            ColorDict.Add("white", "0110");
+            ColorDict.Add("orange", "1100");
+            ColorDict.Add("light-blue", "0111");
+            ColorDict.Add("gray", "1110");
+            ColorDict.Add("purple", "0000");
+            //1111 - Reserved
+            SymbolDict.Add(">", "0001");
+            SymbolDict.Add("<", "0010");
+            SymbolDict.Add("^", "0100");
+            SymbolDict.Add("v", "1000");
+            SymbolDict.Add("$", "0011");
+            SymbolDict.Add("X", "0110");
+            SymbolDict.Add("!", "1100");
+            SymbolDict.Add("@", "0111");
+            SymbolDict.Add("#", "1110");
+            SymbolDict.Add(" ", "0000");
+            //1111 - Reserved
+        }
         public override string ToString()
         {
             return Symbol.ToString();
         }
+        public Cell()
+        {
+            if (SymbolDict.Count == 0 && ColorDict.Count == 0)
+            {
+                CreateDict();
+            }
+        }
 
+        public byte GetCellByte()
+        {
+            return Byte.Parse(ColorDict[Color] + SymbolDict[Symbol]);
+        }
     }
 }
