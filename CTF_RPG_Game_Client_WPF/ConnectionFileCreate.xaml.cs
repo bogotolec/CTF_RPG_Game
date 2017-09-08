b59f8e906eb492b339cf49dccaec0fde62c7fcf6
bogotolec <bogotolec@inbox.ru>
2017-09-08 12:10:25 +0700

    Начал делать GUI

---
+++ CTF_RPG_Game_Client_WPF/ConnectionFileCreate.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTF_RPG_Game_Client_WPF
+{
+    /// <summary>
+    /// Interaction logic for ConnectionFileCreate.xaml
+    /// </summary>
+    public partial class ConnectionFileCreate : Window
+    {
+        public ConnectionFileCreate()
+        {
+            InitializeComponent();
+            ButtonCreate.Click += ButtonCreate_Click;
+        }
+
+        private void ButtonCreate_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress IP;
+            UInt16 Port;
+            if (IPAddress.TryParse(ServerIpText.Text, out IP) && UInt16.TryParse(ServerPortText.Text, out Port))
+            {
+                File.Create("connection.cfg");
+                File.WriteAllText("connection.cfg", ServerIpText.Text + "\n" + ServerPortText);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect IP address or port", "ERROR", MessageBoxButton.OK);
+            }
+        }
+    }
+}
